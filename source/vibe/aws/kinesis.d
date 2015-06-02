3966e994b79f3a6ae52750932b6a038da32fb5e9
Yannick Koechlin <yannick@koechlin.info>
2015-06-02 15:06:07 +0200

    - method name as enum - destroy json request once the request returns

--- source/vibe/aws/kinesis.d
+++ source/vibe/aws/kinesis.d
@@ -18,26 +18,29 @@ class Kinesis : AWSClient
         super("kinesis." ~ region ~ ".amazonaws.com", region, "kinesis", credsSource);
     }
 
+    enum string putRecordName = apiVersion ~ ".PutRecord";
     auto putRecord(Record rec)
     {
         //  PartitionKey: Determines which shard in the stream the data record is assigned to. Partition keys are Unicode strings with a maximum length limit of 256 characters for each key. Amazon Kinesis uses the partition key as input to a hash function that maps the partition key and associated data to a specific shard. Specifically, an MD5 hash function is used to map partition keys to 128-bit integer values and to map associated data records to shards. As a result of this hashing mechanism, all data records with the same partition key will map to the same shard within the stream.
         auto j = rec.toJson();
         j["StreamName"] = this.stream;
-        import std.stdio;
 
-        auto res = this.doRequest( apiVersion ~ ".PutRecord", j);
+        auto res = this.doRequest( putRecordName, j);
+        j.destroy();
         Json ret = res.responseBody();
         res.destroy();
         return ret;
     }
 
+    enum string putRecordsName = apiVersion ~ ".PutRecords";
     auto putRecords(Record[] records)
     {
-        auto j = Json.emptyObject;
+        auto j =  Json.emptyObject;
         j["StreamName"] = this.stream;
         j["Records"] = records.map!(r => r.toJson()).array;
 
-        auto res = this.doRequest( apiVersion ~ ".PutRecords", j);
+        auto res = this.doRequest( putRecordsName, j);
+        j.destroy();
         Json ret = res.responseBody();
         res.destroy();
         return ret;
