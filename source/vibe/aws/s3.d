73ff8243d9bcad2cfe31ccbec4af8c201eb12c7e
Manuel Frischknecht <manuel.frischknecht@bfh.ch>
2015-11-08 19:12:44 +0100

    Added a simple S3 frontent for PUT requests

---
+++ source/vibe/aws/s3.d
@@ -0,0 +1,29 @@
+﻿module vibe.aws.s3;
+
+import vibe.d;
+
+import vibe.aws.aws;
+import vibe.aws.credentials;
+import vibe.aws.sigv4;
+
+class S3 : AWSClient
+{
+private:
+    string bucket;
+
+public:
+    this(string bucket, string region, AWSCredentialSource credsSource)
+    {
+        this.bucket = bucket;
+        super(bucket ~ ".s3-" ~ region ~ ".amazonaws.com/", region, "s3", credsSource);
+    }
+
+    void put(string resource, RandomAccessStream input, 
+             string contentType = "application/octet-stream", ulong chunkSize = 512*1024
+            )
+    {
+        InetHeaderMap headers;
+        headers["Content-Type"] = contentType;
+        doRESTUpload(HTTPMethod.PUT, resource, headers, input, input.size, chunkSize);
+    }
+}
